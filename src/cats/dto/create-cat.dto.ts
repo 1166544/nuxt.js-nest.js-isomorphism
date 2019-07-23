714b9fa0511c2c4fd33563aefb5a890edc5a4eb0
James Liauw <jamesliauw@163.com>
2019-07-23 09:06:09 +0800

    feat: add common

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -7,12 +7,30 @@ import { IsInt, IsString } from 'class-validator';
  * @class CreateCatDto
  */
 export class CreateCatDto {
+	/**
+	 * name field
+	 *
+	 * @type {string}
+	 * @memberof CreateCatDto
+	 */
 	@IsString()
 	public readonly name: string;
 
+	/**
+	 * age field
+	 *
+	 * @type {number}
+	 * @memberof CreateCatDto
+	 */
 	@IsInt()
 	public readonly age: number;
 
+	/**
+	 * bread
+	 *
+	 * @type {string}
+	 * @memberof CreateCatDto
+	 */
 	@IsString()
 	public readonly breed: string;
 }
