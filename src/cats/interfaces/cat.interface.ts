1fd1fc2792dfb03f54a8f48be5b12141ef47ab03
James Liauw <jamesliauw@163.com>
2019-07-24 19:44:22 +0800

    feat: add common

--- src/cats/interfaces/cat.interface.ts
+++ src/cats/interfaces/cat.interface.ts
@@ -5,7 +5,27 @@
  * @interface ICat
  */
 export interface ICat {
+	/**
+	 * name field
+	 *
+	 * @type {string}
+	 * @memberof ICat
+	 */
 	readonly name: string;
+
+	/**
+	 * age field
+	 *
+	 * @type {number}
+	 * @memberof ICat
+	 */
 	readonly age: number;
+
+	/**
+	 * breed field
+	 *
+	 * @type {string}
+	 * @memberof ICat
+	 */
 	readonly breed: string;
 }
