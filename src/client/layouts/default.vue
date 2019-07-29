4d5a703d5617d80fffacf67ad7fcb3ce7301082c
James Liauw <jamesliauw@163.com>
2019-07-29 17:43:56 +0800

    feat: encup router folder

--- src/client/layouts/default.vue
+++ src/client/layouts/default.vue
@@ -185,78 +185,78 @@
 </template>
 <script>
 export default {
-  data() {
-    return {
-      appName: 'Boiler',
-      drawer: true,
-      fixed: false,
-      analyticsItems: [
-        {
-          icon: 'dashboard',
-          title: 'Dashboard',
-          link: '/dashboard/overview',
-        },
-        {
-          icon: 'event',
-          title: 'Events',
-          link: '',
-        },
-        {
-          icon: 'comment',
-          title: 'Notifications',
-          link: '',
-        },
-      ],
-      developItems: [
-        {
-          icon: 'supervisor_account',
-          title: 'Authentification',
-          link: '',
-        },
-        {
-          icon: 'storage',
-          title: 'Database',
-          link: '',
-        },
-        {
-          icon: 'perm_media',
-          title: 'Storage',
-          link: '',
-        },
-        {
-          icon: 'public',
-          title: 'Hosting',
-          link: '',
-        },
-        {
-          icon: 'functions',
-          title: 'Functions',
-          link: '',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      tabs: null,
-      tabsItems: [
-        {
-          id: 1,
-          title: 'Indicators',
-          link: 'indicators',
-        },
-        {
-          id: 2,
-          title: 'Backup',
-          link: 'backup',
-        },
-        {
-          id: 3,
-          title: 'Logs',
-          link: 'logs',
-        },
-      ],
-      menuItems: ['Vue', 'NodeJS', 'Laravel'],
-    };
-  },
+	data() {
+		return {
+			appName: 'Boiler',
+			drawer: true,
+			fixed: false,
+			analyticsItems: [
+				{
+					icon: 'dashboard',
+					title: 'Dashboard',
+					link: '/dashboard/overview'
+				},
+				{
+					icon: 'event',
+					title: 'Events',
+					link: '/dashboard'
+				},
+				{
+					icon: 'comment',
+					title: 'Notifications',
+					link: '/inspire'
+				}
+			],
+			developItems: [
+				{
+					icon: 'supervisor_account',
+					title: 'Authentification',
+					link: '/test'
+				},
+				{
+					icon: 'storage',
+					title: 'Database',
+					link: '/test'
+				},
+				{
+					icon: 'perm_media',
+					title: 'Storage',
+					link: '/test'
+				},
+				{
+					icon: 'public',
+					title: 'Hosting',
+					link: '/test'
+				},
+				{
+					icon: 'functions',
+					title: 'Functions',
+					link: '/test'
+				}
+			],
+			miniVariant: false,
+			right: true,
+			rightDrawer: false,
+			tabs: null,
+			tabsItems: [
+				{
+					id: 1,
+					title: 'Indicators',
+					link: 'indicators'
+				},
+				{
+					id: 2,
+					title: 'Backup',
+					link: 'backup'
+				},
+				{
+					id: 3,
+					title: 'Logs',
+					link: 'logs'
+				}
+			],
+			menuItems: ['Vue', 'NodeJS', 'Laravel']
+		};
+	}
 };
 </script>
